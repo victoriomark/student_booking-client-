13032d42b467e3cf1e50eb5e096012e10fc2a456
Your Name <you@example.com>
2025-02-18 06:55:49 +0800

    update nasa 90% na

---
+++ app/auth_logouts/admin_logout.php
@@ -0,0 +1,1 @@
+<?phpsession_start();session_destroy();session_unset();header('Location: ../views/login.php');
--- app/views/Dashboard.php
+++ app/views/Dashboard.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'Admin') {
+    header('Location: ./login.php');
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -10,6 +17,8 @@
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
         <link href="../../css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     </head>
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
@@ -26,7 +35,7 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Logout</a></li>
+                        <li><a class="dropdown-item" href="../auth_logouts/admin_logout.php">Logout</a></li>
                     </ul>
                 </li>
             </ul>
@@ -45,7 +54,15 @@
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">Booking</div>
+                                    <div class="card-body d-flex flex-column justify-content-center align-items-center ">
+                                        <h6>Total Booking</h6>
+                                        <?php
+                                        include_once  '../controllers/bookingController.php';
+                                        \controllers\bookingController::countBooking();
+                                        ?>
+
+                                    </div>
+
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="#">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
@@ -53,8 +70,16 @@
                                 </div>
                             </div>
                             <div class="col-xl-3 col-md-6">
-                                <div class="card bg-warning text-white mb-4">
-                                    <div class="card-body">Students</div>
+                                <div class="card bg-secondary text-white mb-4">
+                                    <div class="card-body">
+                                        <div class="card-body d-flex flex-column justify-content-center align-items-center ">
+                                            <h6>Total Students</h6>
+                                            <?php
+                                            include_once  '../controllers/studentController.php';
+                                            \controllers\studentController::countStudent();
+                                            ?>
+                                        </div>
+                                    </div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="#">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
@@ -63,7 +88,13 @@
                             </div>
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-danger text-white mb-4">
-                                    <div class="card-body">Announcement</div>
+                                    <div class="card-body d-flex flex-column justify-content-center align-items-center gap-3 ">
+                                        <h6>Total Announcement</h6>
+                                        <?php
+                                        include_once  '../controllers/announcementController.php';
+                                        \controllers\announcementController::countAnnouncement();
+                                        ?>
+                                    </div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="#">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
@@ -79,25 +110,21 @@
                             <div class="card-body">
                                 <table id="datatablesSimple">
                                     <thead>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
-                                        </tr>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>First Name</th>
+                                        <th>Last Name</th>
+                                        <th>Middle Name</th>
+                                        <th>Email</th>
+                                        <th>Student Id</th>
+                                        <th>Action</th>
+                                    </tr>
                                     </thead>
                                     <tbody>
-
-                                        <tr>
-                                            <td>Finn Camacho</td>
-                                            <td>Support Engineer</td>
-                                            <td>San Francisco</td>
-                                            <td>47</td>
-                                            <td>2009/07/07</td>
-                                            <td>$87,500</td>
-                                        </tr>
+                                    <?php
+                                    include_once  '../controllers/studentController.php';
+                                    \controllers\studentController::displayToTable();
+                                    ?>
                                     </tbody>
                                 </table>
                             </div>
@@ -122,5 +149,6 @@
         <script src="../../js/scripts.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
         <script src="../../js/datatables-simple-demo.js"></script>
+        <script src="../JQUERY/student.js"></script>
     </body>
 </html>
